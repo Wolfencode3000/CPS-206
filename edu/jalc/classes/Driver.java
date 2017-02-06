ad01c5dba1f3c1367dadf4582c5e7b8171902a9f
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-06 08:55:16 -0600

    Classes

---
+++ edu/jalc/classes/Driver.java
@@ -0,0 +1,9 @@
+package edu.jalc.classes;
+
+public class Driver{
+   public static void main (String...args){
+   
+   UselessClass uselessClass;new UselessClass();
+   
+   }
+}
---
+++ edu/jalc/classes/UselessClass.java
@@ -0,0 +1,16 @@
+package edu.jalc.classes;
+
+class UselessClass{}
+   
+/* Visibility classes
+public -  everyone can access
+private - only class can access
+protected - 
+packge private - package and subclasses can access
+
+Most restricted
+private
+package private
+protected
+public
+**/ 

d22c7b2acc7479381b521f550b964a20bdb6983e
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-21 08:39:38 -0600

    2-21-17 notes

---
+++ edu/jalc/inclass/notes/2-21-17.java
@@ -0,0 +1,36 @@
+www.lucidchart.com
+
+do UMLs
+(programming charts)
+
+symbols
+minus symbol -
+   private
+plus symbol  +
+   public
+paranthese   :
+   indicates a method and return (method:return)
+   
+Ven diagram
+overlaying circle diagram
+a intersect b intersect c
+
+when to abstract information to keep from reusing it
+
+bottle ----------SodaBottle
+               |   label
+               |   lid
+   capacity    |
+   height      |-BeerBottle
+   width
+   
+Inheritance Triangle/Diamond?
+trying to make a strict language more useable
+
+pour {Bottle,Rain}
+   pourable p-new Rain
+   pourable p-new Bottle
+
+oxygen sensor for a motor vehicle
+14.7 air/fuel ratio
+sensor reach 660 degrees Fahrenheit

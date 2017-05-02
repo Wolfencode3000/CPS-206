1d6134ebf44c5b47ff378264310f775bf8563006
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-05-02 08:53:31 -0500

    observer pattern

---
+++ edu/jalc/builder patterns/observer/learning observer.java
@@ -0,0 +1,19 @@
+main 
+log.warn(when,---)
+
+
+Logger
+observer.observe(this)
+Arraylist<observer>observer = new arraylist
+observers.parallelstream().forEach((observer) -> observer.observe).this);
+
+interface Observer{
+public void observe(Logger logger)
+}
+
+
+warns
+
+error
+
+fatal
